ae5037f545cf637c2838b7eb1919f81dad56ee11
Oveatrint <kbaiyewu@gmail.com>
2026-01-01 10:55:04 +0100

    refactor: added some error checking

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from datetime import datetime
 import os
 import itertools
 
-
-STATUS = ["todo", "in-progress", "done"]
+STATUS = ["in-progress", "done"]
 key = itertools.count()
 
 def load_json() -> list:
@@ -16,26 +15,17 @@ def load_json() -> list:
 
     # Loads JSON file for editing from the file object
     with open("database.json") as read_json:
-        tasks = json.load(read_json)
+        jobs = json.load(read_json)
 
-    return tasks
+    return jobs
 
-def task_id_is_invalid(task_id) -> bool:
-    try:
-        int(task_id)
-    except ValueError:
-        return True
-    return False
-
-def add_task(desc: str) -> None:
-    tasks = load_json()
-
+def add_task(task_list: list,desc: str) -> None:
     # Opens JSON file to write into it
     with open("database.json", "w") as write_json:
         created_at = datetime.now()
         created_at = created_at.strftime("%Y/%m/%d %H:%M:%S")
 
-        task = {
+        job = {
             "id": next(key),
             "description": desc,
             "status": "to-do",
@@ -43,113 +33,77 @@ def add_task(desc: str) -> None:
             "updatedAt": "-"
         }
         # Adds tasks to a loaded JSON file
-        tasks.append(task)
+        task_list.append(job)
 
         # write the JSON file back into a file object
-        json.dump(tasks, write_json, indent=4)
-    print(f"Task added successfully ID: {task["id"]}")
+        json.dump(task_list, write_json, indent=4)
+    print(f"Task added successfully ID: {job["id"]}")
 
-
-def update_task(task_id: str, desc: str):
-    if task_id_is_invalid(task_id):
-        raise ValueError("Invalid Task ID")
-
-    tasks = load_json()
-    for task in tasks:
-        if task_id == task["id"]:
-            break
-    else:
-        raise ValueError(f"Task with ID {task_id} not found")
-
+def update_task(task_list: list,task_id: int, desc: str):
     with open("database.json","w") as update_json:
-        for task in tasks:
-            if task["id"] == int(task_id):
+        for job in task_list:
+            if job["id"] == task_id:
                 updated_at = datetime.now()
                 updated_at = updated_at.strftime("%Y/%m/%d %H:%M:%S")
 
-                task["description"] = desc
-                task["updatedAt"] = updated_at
-                print("Task Successfully Updated")
-        json.dump(tasks, update_json, indent=4)
+                job["description"] = desc
+                job["updatedAt"] = updated_at
+                break
+        print("Task Successfully Updated")
 
+        json.dump(task_list, update_json, indent=4)
 
-def delete_task(task_id: int):
-    if task_id_is_invalid(task_id):
-        raise ValueError("Invalid User ID")
-
-    tasks = load_json()
+def delete_task(task_list: list, task_id: int):
     with open("database.json","w") as delete_json:
-        for task in tasks:
-            if task["id"] == int(task_id):
-                tasks.remove(task)
+        for job in task_list:
+            if job["id"] == task_id:
+                task_list.remove(job)
                 print("Task successfully deleted")
                 break
-        else:
-            raise ValueError(f"Task with ID {task_id} not found")
+        json.dump(task_list, delete_json, indent=4)
 
-        json.dump(tasks, delete_json, indent=4)
-
-def mark_in_progress(task_id: str):
-    if task_id_is_invalid(task_id):
-        raise ValueError("Invalid Task ID")
-
-    tasks = load_json()
+def mark_in_progress(task_list: list, task_id: int):
     with open("database.json", "w") as mark_json:
-        for task in tasks:
-            if task["id"] == int(task_id):
-                task["status"] = "in-progress"
+        for job in task_list:
+            if job["id"] == task_id:
+                job["status"] = "in-progress"
                 break
-        else:
-            raise ValueError(f"Task with ID {task_id} not found")
+        json.dump(task_list, mark_json, indent= 4)
 
-        json.dump(tasks, mark_json, indent= 4)
-
-
-def mark_done(task_id: str):
-    if task_id_is_invalid(task_id):
-        raise ValueError("Invalid Task ID")
-
-    tasks = load_json()
-    for task in tasks:
-        if task
-
+def mark_done(task_list: list, task_id: str):
     with open("database.json", "w") as mark_json:
-        for task in tasks:
-            if task["id"] == int(task_id):
-                task["status"] = "done"
-        json.dump(tasks, mark_json, indent= 4)
+        for job in task_list:
+            if job["id"] == task_id:
+                job["status"] = "done"
+                break
+        json.dump(task_list, mark_json, indent= 4)
 
 
-def list_task(status = None) -> None:
+def list_task(task_list: list, status = None):
     if status is not None and status not in STATUS:
         raise ValueError("Invalid Status, type **help** to see available statuses")
 
-    if json_is_empty():
-        print("No tasks found...")
-        return None
-
-    tasks = load_json()
     print("=" * 115)
     print(f"{"ID":5} ¦{"DESCRIPTION":^50} ¦{"STATUS":^15} ¦{"TIME CREATED":^19} ¦{"TIME UPDATED":^15}")
     print("=" * 115)
 
     if status is None:
-        for task in tasks:
-            print(f"{task["id"]:<5} ¦{task["description"]:^50} ¦{task["status"]:^15} ¦{task["createdAt"]:^15} ¦{task["updatedAt"]:^15}")
+        for job in task_list:
+            print(f"{job["id"]:<5} ¦{job["description"]:^50} ¦{job["status"]:^15} ¦{job["createdAt"]:^15} ¦{job["updatedAt"]:^15}")
             print("-" * 115)
     elif status is not None:
         count = True
-        for task in tasks:
-            if status == task["status"]:
+        for job in task_list:
+            if status == job["status"]:
                 count = False
                 break
         if count:
             print(f"Task with {status} not present")
             return None
 
-        for task in tasks:
-            if task["status"] == status:
-                print(f"{task["id"]:<5} ¦{task["description"]:^50} ¦{task["status"]:^15} ¦{task["createdAt"]:15} ¦{task["updatedAt"]:^15}")
+        for job in task_list:
+            if job["status"] == status:
+                print(f"{job["id"]:<5} ¦{job["description"]:^50} ¦{job["status"]:^15} ¦{job["createdAt"]:15} ¦{job["updatedAt"]:^15}")
                 print("-" * 115)
 
 
@@ -163,72 +117,70 @@ def helper() -> None:
     print("     *mark-done* => Updates the status of a task to done")
     print("     *exit* => Exits the task manager")
     print("     *help* => Lists all commands")
-    print("     POSSIBLE STATUSES => todo (default), in-progress, done")
-
-
-def check_instruction_valid(line: list) -> bool:
-    commands = ["add", "update", "list", "delete", "mark-in-progress", "mark-done", "exit", "help"]
-    operand = line[0]
-
-    if operand not in commands:
-        raise ValueError("Invalid command")
-
-    if operand  == "add" and len(line) < 2:
-        raise
-    if operand == "update":
-        if len(line) < 3:
-            print("Invalid argument")
-
-def main():
-    pass
+    print("     POSSIBLE STATUSES => in-progress, done")
 
 if __name__ == "__main__":
     print("Welcome to the Task Manager, type help to check the commands")
     print("-" * 60)
 
     while True:
-        instruction = input("task-cli ")
-        parts = instruction.split(" ")
-        command = parts[0].lower()
+        tasks = load_json()
+        instructions = input("task cli ")
 
+        list_of_instructions = instructions.split(" ")
+        command = list_of_instructions[0]
+        args = list_of_instructions[1:]
 
+        if len(tasks) == 0 and command in ["update", "delete", "mark-in-progress", "mark-done", "list"]:
+            print("No tasks available, add a task before using this command")
+            continue
 
-        if command == "add" :
-            description = " ".join(parts[1:])
-            add_task(description)
-
-        elif command == "update":
-            t_id = parts[1]
-            description =  " ".join(parts[2:])
-            update_task(t_id, description)
-
-        elif command == "delete":
-            if len(parts) != 2:
-                print("Invalid argument")
+        if command in ["update", "delete", "mark-in-progress", "mark-done"]:
+            t_id = args[0]
+            try:
+                t_id = int(t_id)
+            except ValueError:
+                print("Invalid Task ID")
                 continue
-            t_id = parts[1]
-            delete_task(t_id)
 
-        elif command == "list":
-            if len(parts) == 2:
-                t_id = parts[1]
-                list_task(t_id)
-            elif len(parts) == 1:
-                list_task()
+            if t_id < 0:
+                print("Task ID must be greater than 0")
+                continue
+
+            for task in tasks:
+                if t_id == task["id"]:
+                    break
             else:
-                print("Invalid argument")
+                print(f"Task with id {t_id} doesn't exist")
+                continue
 
+        if command in ["mark-done","mark-in-progress", "delete", "list"] and len(args) > 1:
+            print("Invalid Argument")
+            continue
+
+        if command in ["add", "update"]:
+            if command == "add":
+                description = " ".join(args)
+            else:
+                description = " ".join(args[2:])
+
+
+        if command == "add":
+            add_task(tasks,description)
+        elif command == "update":
+            update_task(tasks, t_id, description)
+        elif command == "delete":
+            delete_task(tasks, t_id)
         elif command == "mark-in-progress":
-            if len(parts) != 2:
-                print("Invalid Argument")
-
-            mark_in_progress(parts[1])
-
+            mark_in_progress(tasks, t_id)
         elif command == "mark-done":
-            mark_done(parts[1])
-
+            mark_done(tasks, t_id)
+        elif command == "list":
+            list_task(tasks, )
         elif command == "help":
             helper()
         elif command == "exit":
-            print("Bye, See you later")
+            print("Bye Bye👋")
             break
+        else:
+            print("Invalid command")
